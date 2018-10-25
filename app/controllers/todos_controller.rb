76478980389596bb5860b44f4d1a675c4aded911
Juan Tristan <tristansatx210@gmail.com>
2018-10-25 22:48:52 +0000

    Added Delete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -34,6 +34,13 @@ class TodosController < ApplicationController
     @todos = Todo.all
   end
   
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    flash[:notice] = "Deleted successfully"
+    redirect_to todos_path
+  end
+  
   private
   
   def todo_params
